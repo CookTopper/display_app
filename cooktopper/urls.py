4766649c4b74764878a8d9e097fb0b56addf2eb8
ShutUpPaulo <markes.calado@gmail.com>
2017-10-20 17:07:17 -0200

    Create QRCode front end
    * Created page to show QRCode
    * Created design of the page above
    
    Signed-off-by: ShutUpPaulo <markes.calado@gmail.com>

--- cooktopper/urls.py
+++ cooktopper/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$', views.homepage, name='HomePage'),
 	url(r'^burner/(?P<id>\d+)/$', views.burner, name='burner'),
 	url(r'^program_burner/(?P<burner_id>\d+)/$', views.program_burner, name='program_burner'),
+	url(r'^register/$', views.register, name='register')
 ]

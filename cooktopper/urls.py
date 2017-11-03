ca3c04b74d7097e19a060f934e296f475ca36521
ShutUpPaulo <markes.calado@gmail.com>
2017-11-03 12:09:58 -0200

    Change the front end to Program Burner screen
    Make the components responsive.
    
    Signed-off-by: ShutUpPaulo <markes.calado@gmail.com>

--- cooktopper/urls.py
+++ cooktopper/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.homepage, name='HomePage'),
 	url(r'^burner/(?P<id>\d+)/$', views.burner, name='burner'),
+	url(r'^program_burner/(?P<id>\d+)/$', views.burner, name='program_burner'),
 ]
